b800ff72b96c6bc21fd18ea2a58c83ab8f395229
mohamed hassaan <mmhassaan3@gmail.com>
2024-08-18 01:02:36 +0400

    add person paragraphy feature

--- src/components/person-page/PersonBanner.tsx
+++ src/components/person-page/PersonBanner.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import { FaAngleRight } from "react-icons/fa";
 
 const PersonBanner = ({ personDetails }: { personDetails: PersonDetails }) => {
   const {
@@ -15,8 +16,11 @@ const PersonBanner = ({ personDetails }: { personDetails: PersonDetails }) => {
     external_ids,
     credits,
   } = personDetails;
+  const [readMore, setReadMore] = useState<boolean>(false);
   const paragraphs = biography?.split("\n\n");
+
   console.log(biography);
+  console.log(biography?.slice().length);
   return (
     <article>
       <section>
@@ -28,22 +32,39 @@ const PersonBanner = ({ personDetails }: { personDetails: PersonDetails }) => {
             height={300}
             alt="Banner"
           />
-          <figcaption className="space-y-12">
+          <figcaption className="space-y-12  overflow-hidden">
             <h1 className="text-3xl font-bold tracking-wide">
               {name || "Not available"}
             </h1>
             {biography && (
-              <p className="text-xl font-bold">
-                Biography
-                {paragraphs.map((paragraph, i) => (
-                  <span
-                    key={i}
-                    className="block font-normal text-[16px] mb-4 leading-tight mt-2"
-                  >
-                    {paragraph || "Not available"}
-                  </span>
-                ))}
-              </p>
+              <>
+                <p className="text-xl font-bold">Biography</p>
+                <div
+                  className={`${
+                    readMore ? "h-none" : "h-36"
+                  } relative overflow-hidden !mt-0`}
+                >
+                  {paragraphs.map((paragraph, i) => (
+                    <p
+                      key={i}
+                      className="block font-normal text-[16px] mb-4 leading-tight mt-2 "
+                    >
+                      {readMore ? paragraph : paragraph.slice(0, 600)}
+                      {!readMore && biography?.slice().length >= 600 && (
+                        <>
+                          <div className="absolute inset-x-0 bottom-0 h-6 bg-gradient-to-l from-white to-transparent"></div>
+                          <button
+                            onClick={() => setReadMore(!readMore)}
+                            className=" absolute bottom-0 right-0 p-1 text-blue-500 font-medium"
+                          >
+                            Read More
+                          </button>
+                        </>
+                      )}
+                    </p>
+                  ))}
+                </div>
+              </>
             )}
           </figcaption>
         </figure>
