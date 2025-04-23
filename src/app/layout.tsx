7b20c6c33ae41b7b976db2daf24f094010b870f3
Mohamed Hassaan <mmhassaan3@gmail.com>
2025-04-23 12:35:48 +0400

    add Analytics vercel to app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
 import StoreProvider from "@/lib/redux/StoreProvider";
 import NextTopLoader from "nextjs-toploader";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,13 +13,14 @@ export const metadata: Metadata = {
   metadataBase: new URL("https://movie-night-rtk-next.vercel.app/"),
   title: {
     default: "Movie Night",
-    template: "%s | Movie Night"
+    template: "%s | Movie Night",
   },
-  description: "A Next.js movie database application built with React, Redux Toolkit, and Axios. It offers real-time movie, TV show, and actor search and filtering capabilities. The app uses server-side actions for dynamic data updates and leverages responsive design for an optimized experience across devices. Features include API integration, global state management, and seamless UI interactions.",
+  description:
+    "A Next.js movie database application built with React, Redux Toolkit, and Axios. It offers real-time movie, TV show, and actor search and filtering capabilities. The app uses server-side actions for dynamic data updates and leverages responsive design for an optimized experience across devices. Features include API integration, global state management, and seamless UI interactions.",
   alternates: {
     languages: {
-      'en-US': '/en-US'
-    }
+      "en-US": "/en-US",
+    },
   },
   openGraph: {
     siteName: "Movie Night",
@@ -28,13 +30,13 @@ export const metadata: Metadata = {
       {
         url: "https://i.ibb.co/g7gzctT/opengraph-image.jpg",
         width: 800,
-        height: 600
-      }
-    ]
+        height: 600,
+      },
+    ],
   },
-  twitter: { 
-    card: "summary_large_image"
-  }
+  twitter: {
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({
@@ -64,6 +66,7 @@ export default function RootLayout({
             <Header />
           </header>
           {children}
+          <Analytics />
           <footer className="bg-darkBlue text-white py-6">
             <Footer />
           </footer>
